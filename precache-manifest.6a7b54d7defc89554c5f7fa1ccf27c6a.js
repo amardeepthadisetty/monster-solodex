7b9a47de2504f1a869fb21281713aa3aaa1c02da
Amardeep T <amardeept@annait.net>
2020-05-30 19:39:13 +0530

    Updates

---
+++ precache-manifest.6a7b54d7defc89554c5f7fa1ccf27c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a38262419b1186ffa0e9f3a6985bd50",
+    "url": "/monster-solodex/index.html"
+  },
+  {
+    "revision": "d7aedc25389b47c2b3c9",
+    "url": "/monster-solodex/static/css/main.03ac79e5.chunk.css"
+  },
+  {
+    "revision": "81fa52cb922ed55de099",
+    "url": "/monster-solodex/static/js/2.2faf885d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-solodex/static/js/2.2faf885d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7aedc25389b47c2b3c9",
+    "url": "/monster-solodex/static/js/main.6aa829cd.chunk.js"
+  },
+  {
+    "revision": "ceea5039b6907d68a84e",
+    "url": "/monster-solodex/static/js/runtime-main.8afe5122.js"
+  }
+]);
